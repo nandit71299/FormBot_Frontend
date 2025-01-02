13ddb7f408868b65bcf5171b6e237f90322a5c5c
Sareria Nandit <45124266+nandit71299@users.noreply.github.com>
2025-01-02 23:59:19 +0530

    API UTIL CHANGES

--- src/utils/apiUtil.js
+++ src/utils/apiUtil.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_URL}:${
-  import.meta.env.VITE_API_PORT
-}/api`;
+const API_PORT = import.meta.env?.VITE_API_PORT || "";
+const api_url = import.meta.env.VITE_API_URL;
+const API_URL = `${api_url}:${API_PORT}/api`;
 
 export const verifyToken = async () => {
   try {
